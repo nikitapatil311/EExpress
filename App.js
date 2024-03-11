424474d2124983c3f094061aa1b8e82054b4819c
Nikita S Patil <nikitaspatil311@gmail.com>
2024-03-11 20:15:15 +0530

    add libraries

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> start working on your app!</Text>
+      <Text> Hi, start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
