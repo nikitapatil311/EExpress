5600c771ddef6bc561868e4608bb06a7b63e5321
Nikita S Patil <nikitaspatil311@gmail.com>
2024-03-12 17:49:20 +0530

    add Home

--- navigation/BottomTabNavigation.jsx
+++ navigation/BottomTabNavigation.jsx
@@ -1,11 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
 
+const Tab = createBottomTabNavigator();
 const BottomTabNavigation = () => {
   return (
-    <view>
-      <Text>BottomTabNavigation</Text>
-    </view>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
   );
 };
---
+++ screens/Home.jsx
@@ -0,0 +1,14 @@
+import { Stylesheet, Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <view>
+      <Text>Home</Text>
+    </view>
+  );
+};
+
+export default Home;
+
+const styles = Stylesheet.create({});
