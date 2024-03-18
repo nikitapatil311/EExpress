7e820e8d6a0efd2e90de94ed8f8b24a5b897a6cf
Nikita S Patil <nikitaspatil311@gmail.com>
2024-03-18 18:13:30 +0530

    add welcome

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,12 +1,35 @@
 import { view, Text } from "react-native";
 import React from "react";
 import styles from "./welcome.style";
+import { COLORS, SIZES } from "../../constants";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Feather } from "expo/vector-icons";
 
 const Welcome = () => {
   return (
-    <view style={styles.container}>
-      <Text style={styles.welcomeTxt(COLORS.black)}>Find the most</Text>
-      <Text style={styles.welcomeTxt}> Luxurious Furniture </Text>
+    <view>
+      <view style={styles.container}>
+        <Text style={styles.welcomeTxt(COLORS.black, SIZES.xSmall)}>
+          Find the most
+        </Text>
+        <Text style={styles.welcomeTxt(COLORS > primary, 0)}>
+          Luxurious Furniture
+        </Text>
+      </view>
+      <view style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather name="search" size={24} style={styles.searchIcon}></Feather>
+        </TouchableOpacity>
+        <view>
+          style={styles.searchWrapper}
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onPressIn={() => {}}
+            placeholder="what are you looking for"
+          />
+        </view>
+      </view>
     </view>
   );
 };
--- components/home/welcome.style.js
+++ components/home/welcome.style.js
@@ -5,12 +5,29 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
   },
-  welcomeTxt: (color) => ({
+  welcomeTxt: (color, top) => ({
     fontFamily: "bold",
-    fontSize: SIZES.xxLarge - 5,
+    fontSize: SIZES.xxLarge - 6,
     marginTop: top,
     color: color,
+    marginHorizontal: 12,
   }),
+  searchContainer:{
+    flexDirection:"row",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor:COLORS.secondary 
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium, 
+  },
+  searchIcon:{
+    marginHorizontal: 10,
+    color: COLORS.gray,
+    searchWrapper:{
+        flex:1,
+        backgroundColor
+    }
+  }
 });
 
 export default styles;
