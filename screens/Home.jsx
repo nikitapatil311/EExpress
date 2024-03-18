359574da56496f99b19e81fd860a3953a11532ce
Nikita S Patil <nikitaspatil311@gmail.com>
2024-03-18 12:41:13 +0530

    add home component

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,8 +1,9 @@
-import { Stylesheet, Text, View } from "react-native";
+import { TouchableOpacity, Stylesheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./home.style";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Home = () => {
   return (
@@ -10,9 +11,19 @@ const Home = () => {
       <View style={styles.appBarWrapper}>
         <View style={styles.appBar}>
           <Ionicons name="location-outline" size={24} />
+          <Text style={styles.location}> Shangai chaina</Text>
           <Text> Shangai chaina</Text>
+          <View style={{ alignItems: "flex-end" }}>
+            <View style={styles.cartCout}>
+              <Text style={styles.cartNumber}> 8 </Text>
+            </View>
+            <TouchableOpacity>
+              <Fontisto name="shopping-bag" size={24} />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
+      <ScrollView></ScrollView>
     </SafeAreaView>
   );
 };
--- screens/home.style.js
+++ screens/home.style.js
@@ -15,5 +15,27 @@ const styles = StyleSheet.create({
     justifyContent "space-between",
     alignItems: "center"
   }
+  location:{
+    fontFamily: "semiBold",
+    fontSize: SIZES.medium,
+    color: COLORS.gray
+  }
+  cartCout:{
+    position:"absolute",
+    bottom: 16,
+    width:16,
+    height:16,
+    borderRadius:8,
+    alighItems: "center",
+    backgroundColor: "green",
+    justifyContent: "center",
+    zIndex: 999
+  }
+  cartNumber:{
+    fontFamily:"regular",
+    fontWeight: "600",
+    fontSize: 10,
+    color: COLORS.lightWhite
+  }
 });
 export default styles;
