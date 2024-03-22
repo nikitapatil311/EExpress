c47c6a5be1dcbc00945528cf47ac0b988830bf67
Nikita S Patil <nikitaspatil311@gmail.com>
2024-03-22 12:16:23 +0530

    add welcome

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,11 +1,33 @@
 import { Stylesheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Search = () => {
   return (
     <SafeAreaView>
-      <Text>Search</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather name="search" size={24} style={styles.searchIcon} />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onPressIn={() => navigation.navigate("search")}
+            placeholder="what are you looking for"
+          />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.searchBtn}>
+            <Ionicons
+              name="camera-outline"
+              size={SIZES.xLarge}
+              color={Colors.offwhite}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- screens/home.style.js
+++ screens/home.style.js
@@ -13,24 +13,24 @@ const styles = StyleSheet.create({
   appBar:{
     flexDirection: "row",
     justifyContent "space-between",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
   location:{
     fontFamily: "semiBold",
     fontSize: SIZES.medium,
     color: COLORS.gray
-  }
+  },
   cartCout:{
     position:"absolute",
     bottom: 16,
     width:16,
     height:16,
     borderRadius:8,
-    alighItems: "center",
+    alignItems: "center",
     backgroundColor: "green",
     justifyContent: "center",
     zIndex: 999
-  }
+  },
   cartNumber:{
     fontFamily:"regular",
     fontWeight: "600",
